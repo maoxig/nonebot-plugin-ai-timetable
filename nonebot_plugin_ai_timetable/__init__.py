dee9d6bff2b3be542c72ab14aea6984662bb9d55
Xenophon <123555887+maoxig@users.noreply.github.com>
2023-03-04 21:01:47 +0800

    Add files via upload

---
+++ nonebot_plugin_ai_timetable/__init__.py
@@ -0,0 +1,149 @@
+import httpx
+import re
+from nonebot_plugin_apscheduler import scheduler
+from .utils import*
+from nonebot_plugin_htmlrender import get_new_page
+from nonebot.plugin import PluginMetadata
+from nonebot.matcher import Matcher
+from nonebot.params import RegexMatched, ArgStr
+from nonebot import on_command, on_regex
+from nonebot.adapters.onebot.v11 import Bot, MessageSegment, MessageEvent, GroupMessageEvent
+from nonebot import require, get_bot
+
+require('nonebot_plugin_htmlrender')
+logger.opt(colors=True).info(
+    "已检测到软依赖<y>nonebot_plugin_apscheduler</y>, <g>开启定时任务功能</g>"
+    if scheduler
+    else "未检测到软依赖<y>nonebot_plugin_apscheduler</y>，<r>禁用定时任务功能</r>"
+)
+__plugin_meta__ = PluginMetadata(
+    name="小爱课表",
+    description="一键导入课表、查看课表、提醒上课、查询课程",
+    usage="课表帮助",
+)
+mytable = on_regex(r'^(小爱|我的|本周|下周)(课表)', priority=1, block=False)
+newtable = on_command('导入课表', priority=1, block=False, aliases={'创建课表'})
+tablehelp = on_command("课表帮助", priority=20, block=True,
+                       aliases={"课表介绍", "课表怎么用"})
+someday_table = on_regex(
+    r'^(((今|明|昨|后)(天|日))|(星期|周)(一|二|三|四|五|六|日|天))(课表|课程|((上|有)(什么|啥)课))', priority=20, block=True)
+add_alock_someday = on_regex(
+    r'^(订阅|提醒)((周|星期)(一|二|三|四|五|六|日|天))(课程|课表)', priority=20, block=True)
+add_alock_morningcalss = on_regex(
+    r'^(订阅|提醒)早八', priority=20, block=True)
+remove_alock_someday = on_regex(
+    r'^(取消)(订阅|提醒)((周|星期)(一|二|三|四|五|六|日|天))(课程|课表)', priority=20, block=True)
+
+@tablehelp.handle()
+async def _(matcher: Matcher, bot: Bot, event: MessageEvent):
+    await tablehelp.finish(__usage__)
+__usage__ = "小爱课表帮助:\n#我的/本周课表:获取本周课表,也可以是下周\n#导入课表:使用小爱课程表分享的链接一键导入\n#某日课表:获取某日课表，如今日课表、周一课表\n#更新课表:更新本地课表信息\n#订阅/取消订阅xx:可以订阅某天的课表，在前一天晚上10点推送"
+
+
+@mytable.handle()  # 本/下 周完整课表
+async def _(bot: Bot, event: MessageEvent,key:str=RegexMatched()):
+    uid = event.get_user_id()
+    if uid in userdata:
+        async with get_new_page(viewport={"width": 1000, "height": 1200}) as page:
+            page.on("response", lambda response: geturl(response))
+            await page.goto(userdata[uid], wait_until='networkidle')
+            # 这里使小爱课程表的导入按钮隐藏，防止遮挡课表
+            await page.evaluate('var t = document.querySelector("#root>div>div.importSchedule___UjEKt>div.footer___1iAis.toUp___2mciB"); t.style.display = "none"')
+            if '下' in key:#如果命令中有下字，就点击下一周的按钮
+                await page.click("#schedule-view > div.header___26sI1 > div.presentWeek___-o65e > div.rightBtn___2ZhSY")
+            pic = await page.screenshot(full_page=True, path="./mytable.png")
+            await mytable.finish(MessageSegment.image(pic))
+
+    else:
+        await mytable.finish('你还没有导入课表，发送\\导入课表来导入吧！', at_sender=True)
+# 导入课表
+
+real_url = "寄"
+
+
+def geturl(response):
+    global real_url
+    real_url = str(response.url) if re.match(
+        real_url_re, response.url) else real_url
+
+
+@newtable.got('key', '请发送小爱课程表导出的链接，发送\\取消以退出')  # 导入课表
+async def _(matcher: Matcher, bot: Bot, event: MessageEvent, key: str = ArgStr()):
+    uid = event.get_user_id()
+    url = str(key)
+    log_debug("导入url", url)
+    if re.match(base_url_re, url):  # 用户发送的链接匹配
+        async with get_new_page(viewport={"width": 1000, "height": 1200}) as page:
+            page.on("response", lambda response: geturl(response))
+            await page.goto(url, wait_until='networkidle')
+            global real_url
+            log_debug("response.url", real_url)
+            async with httpx.AsyncClient() as r:
+                res = await r.get(real_url)
+                usertable.update({uid: res.json()})
+                usertable[uid]["data"]["courses"].sort(
+                    key=lambda x: int(x["sections"][0]))  # 这里要对课表排序，否则打印课表时不会按时间顺序来
+                write_table()
+        if uid in userdata:  # 这里保存的是小爱课程表分享的链接，就可以定时通过这里的链接更新本地课表，但是还不清楚链接是否会失效
+            userdata.update({uid: url})
+            write_data()
+            await newtable.finish("你已经导入过课表了，咱帮你更新课表数据了", at_sender=True)
+        elif uid not in userdata:
+            userdata.update({uid: url})
+            write_data()
+            await newtable.finish("成功导入课表！快发送课表帮助来查看功能吧！", at_sender=True)
+    else:
+        await newtable.finish("出错了，请检查链接是否正确", at_sender=True)
+
+
+@someday_table.handle()  # 某天的课表
+async def _(matcher: Matcher, bot: Bot, event: MessageEvent, key: str = RegexMatched()):
+    uid = event.get_user_id()
+    if uid not in userdata:
+        await someday_table.finish('你还没有导入课表，发送\\导入课表来导入吧！', at_sender=True)
+    else:
+        await someday_table.finish(table_msg(key, uid), at_sender=True)
+
+
+#-----------以下为定时任务----------------#
+@add_alock_someday.handle()
+async def _(matcher: Matcher, bot: Bot, event: GroupMessageEvent, key: str = RegexMatched()):
+    uid = event.get_user_id()
+    gid = event.group_id
+    if uid not in userdata:
+        await add_alock_someday.finish('你还没有导入课表，发送\\导入课表来导入吧！', at_sender=True)
+    else:
+        if scheduler:
+            send_day = (weekday_int(key)+5) % 7
+            scheduler.add_job(post_alock, "cron", hour=22, id=str(
+                uid+"post_alock"+str(send_day)), args=[key, uid, gid], day_of_week=send_day)
+            await add_alock_someday.finish("定时提醒添加成功！", at_sender=True)
+        else:
+            await add_alock_someday.finish("apscheduler插件未载入，无法添加定时提醒", at_sender=True)
+
+@remove_alock_someday.handle()
+async def _(matcher: Matcher, bot: Bot, event: GroupMessageEvent, key: str = RegexMatched()):
+    uid = event.get_user_id()
+    gid = event.group_id
+    if uid not in userdata:
+        await add_alock_someday.finish('你还没有导入课表，发送\\导入课表来导入吧！', at_sender=True)
+    else:
+        if scheduler:
+            send_day = (weekday_int(key)+5) % 7
+            if scheduler.get_job(str(uid+"post_alock"+str(send_day))):
+                scheduler.remove_job(str(uid+"post_alock"+str(send_day)))
+                await remove_alock_someday.finish("定时提醒删除成功！", at_sender=True)
+            else:
+                await remove_alock_someday.finish("出错了,好像没有订阅过这天的课表呢", at_sender=True)
+        else:
+            await remove_alock_someday.finish("apscheduler插件未载入，无法删除定时提醒", at_sender=True)
+            
+            
+async def post_alock(*args):
+    uid = args[1]
+    key = args[0]
+    gid = args[2]
+    if '一' in key:
+        key = "明日课表"  # 发送周一课表时是周日，所以要发送的其实是明日课表
+    msg = table_msg(key=key, uid=uid)
+    await get_bot().send_group_msg(group_id=gid, message=MessageSegment.at(int(uid))+msg)
---
+++ nonebot_plugin_ai_timetable/utils.py
@@ -0,0 +1,89 @@
+import os
+import json
+import time
+import datetime
+from nonebot import logger
+
+
+def log_debug(command: str, info: str):
+    logger.opt(colors=True).debug(f'<u><y>[{command}]</y></u>{info}')
+
+
+real_url_re = r'^(https://i\.ai\.mi\.com/course-multi/table\?)*(ctId=)\d+(&userId=)\d*[1-9]\d*(&deviceId=)[0-9a-zA-Z]*(&sourceName=course-app-browser)'
+base_url_re = r'https://cdn\.cnbj1\.fds\.api\.mi-img.com/miai-fe-aischedule-wx-redirect-fe/redirect.html\?linkToken=.+'
+# 写入用户信息
+
+
+def write_data() -> None:
+    with open("data/ai_timetable/userdata.json", 'w', encoding='utf-8') as f:
+        json.dump(userdata, f)
+
+
+# 读取用户信息
+if os.path.exists("data/ai_timetable/userdata.json"):
+    with open("data/ai_timetable/userdata.json", 'r', encoding='utf-8') as f:
+        userdata = json.load(f)
+else:
+    if not os.path.exists("data/ai_timetable"):
+        os.makedirs("data/ai_timetable")
+    userdata = {}
+    write_data()
+
+
+
+# 读取用户url
+
+
+def write_table() -> None:
+    with open("data/ai_timetable/usertable.json", 'w', encoding='utf-8') as f:
+        json.dump(usertable, f,ensure_ascii=False)
+
+
+if os.path.exists("data/ai_timetable/usertable.json"):
+    with open("data/ai_timetable/usertable.json", 'r', encoding='utf-8') as f:
+        usertable = json.load(f)
+else:
+    if not os.path.exists("data/ai_timetable"):
+        os.makedirs("data/ai_timetable")
+    usertable = {}
+    write_table()
+
+def weekday_int(key)-> int:
+    cn2an = {'今': datetime.datetime.now().weekday()+1, '明': datetime.datetime.now().weekday()+2, '昨': datetime.datetime.now().weekday(), '后': datetime.datetime.now().weekday()+3, '一': 1, '二': 2, '三': 3, '四': 4, '五': 5, '六':6,'日':7,'天':7}
+    for i in cn2an.keys():
+        if i in key:
+            return cn2an[i]
+    return 7
+    
+    
+        
+def table_msg(key,uid)->str:
+    presentweek = int((time.time() - int(
+        usertable[uid]["data"]["setting"]['startSemester'][0:10]))//604800)+1  # 这里算出的结果是小数，转换成整数
+    someday = weekday_int(key)
+    # 日期变时周数也会变
+    if someday < 0:
+        someday += 7
+        presentweek -= 1
+    elif someday > 7:
+        someday -= 7
+        presentweek += 1
+        # 构造发送的信息
+    msg = '你要的课表来咯喵:\n'
+    for i in range(len(usertable[uid]["data"]["courses"])):
+        if(usertable[uid]["data"]["courses"][i]["day"] == someday) and (str(presentweek) in usertable[uid]["data"]["courses"][i]["weeks"].split(',')):
+            sections = usertable[uid]["data"]["courses"][i]["sections"].split(
+                ",")
+            startsection = int(sections[0])
+            endsection = int(sections[-1])
+            starttime = eval(usertable[uid]["data"]["setting"]["sectionTimes"])[
+                startsection-1]["s"]
+            endtime = eval(usertable[uid]["data"]["setting"]["sectionTimes"])[
+                endsection-1]["e"]
+            msg = msg+'\n'+'#'+starttime+'-'+endtime+'\n' + \
+                usertable[uid]["data"]["courses"][i]["name"] + '\n@' + \
+                usertable[uid]["data"]["courses"][i]["position"]
+    return msg            
+
+
+
